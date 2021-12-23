ef28ca89b4f90cd1b53dabc898be2413eabee276
peterfajemisin-homeoffice <peter.fajemisin@digital.homeoffice.gov.uk>
2021-12-23 11:59:39 +0000

    wip connecting controller to service to repositort.

--- src/main/java/io/digital/patterns/pscms/form/service/PostSeizureCaseMgmntService.java
+++ src/main/java/io/digital/patterns/pscms/form/service/PostSeizureCaseMgmntService.java
@@ -2,6 +2,7 @@ package io.digital.patterns.pscms.form.service;
 
 import io.digital.patterns.pscms.form.service.models.PostSeizureEvent;
 import io.digital.patterns.pscms.form.service.repository.PostSeizureEventRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,12 @@ public class PostSeizureCaseMgmntService {
     @Autowired
     PostSeizureEventRepository postSeizureEventRepository;
 
-    public Optional<PostSeizureEvent> getEventByBusinessKey(String businessKey){
-        return postSeizureEventRepository.findById(businessKey);
+    public JSONObject getEventByBusinessKey(String businessKey){
+        PostSeizureEvent event = postSeizureEventRepository.findById(businessKey).get();
+        //Pojo to Json
+        JSONObject object = new JSONObject(event);
+        System.out.println(object);
+        return object;
     }
 
 }
